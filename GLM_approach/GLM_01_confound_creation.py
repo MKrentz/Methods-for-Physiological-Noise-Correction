2d2f22899eac5059e6599fbb988f1b028bde2a4e
MKrentz <krentz.martin@gmail.com>
2023-06-19 15:07:27 +0200

    Overhauled confound selection to make it consistent with other approaches

--- GLM_approach/GLM_01_confound_creation.py
+++ GLM_approach/GLM_01_confound_creation.py
@@ -8,11 +8,11 @@ Created on Sun Jan 10 15:46:30 2021
 
 import numpy as np
 import glob
-from Subject_Class import Subject
+from Subject_Class_new import Subject
 import pandas as pd
 
-FMRIPREP_PATH = "/project/3013068.03/derivate/fmriprep/"
-BASEPATH = '/project/3013068.03/test/GLM_approach/'
+FMRIPREP_PATH = "/project/3013068.03/fmriprep_test/"
+BASEPATH = '/project/3013068.03/physio_revision/GLM_approach/'
 
 part_list = glob.glob(BASEPATH + 'sub-*')
 part_list.sort()
@@ -25,79 +25,79 @@ for subject_long in part_list:
     sub_id = subject_long[-7:]
     sub = Subject(sub_id)
 
-    ses_nr = 3 if sub_id in stress_list else 2
+    ses_nr = 2 if sub_id in stress_list else 1
 
-    fmriprep_confounds = sub.get_confounds(session = ses_nr-1, run = 2, task = 'RS')
+    # LOAD ALL FMRIPREP CONFOUNDS
+    fmriprep_confounds = sub.get_confounds(session=ses_nr, run=2, task='RS')
 
-    # Selection of nuisance regressors from fmriprep-confound file
+    # SELECT COSINES FOR TEMPORAL DETRENDING
     fmriprep_confound_selection = fmriprep_confounds[['cosine00', 'cosine01', 'cosine02', 'cosine03', 'cosine04',
-                                                      'cosine05']]
+                                                      'cosine05']].copy()
+    output_confound_file = BASEPATH + '{0}/confounds/rs_confounds_{0}_ses-0{1}_run-02_cosines.csv'.format(sub_id, str(ses_nr))
+    fmriprep_confound_selection.to_csv(output_confound_file,
+                                      sep=' ',
+                                      header=None,
+                                      index=False)
 
-    fmriprep_confounds_trimmed = fmriprep_confound_selection
+    # SELECTION OF RELEVANT ACOMPCOR COMPONENTS
+    acompcor_confounds_trimmed = sub.get_acompcor_confounds(session=ses_nr, run=2, task='RS', number_regressors=5)
+    output_acompcor_file = BASEPATH + f'{sub_id}/confounds/rs_confounds_{sub_id}_ses-0{ses_nr}_run-02_acompcor.csv'
+    acompcor_confounds_trimmed.to_csv(output_acompcor_file,
+                                      sep=' ',
+                                      header=None,
+                                      index=False)
 
-    output_confound_file = BASEPATH + '{0}/confounds/Resting_State_confounds_all_{0}_ses-0{1}_run-02.txt'.format(sub_id, str(ses_nr))
-    fmriprep_confounds_trimmed.to_csv(output_confound_file, sep = ' ', header = None, index = False)
+    for acompcor_confound in acompcor_confounds_trimmed:
+        acompcor_confounds_trimmed[acompcor_confound].to_csv(BASEPATH + f'{sub_id}/confounds/{acompcor_confound}')
 
-    #Selection of aCompCor components
-    aCompCor_selection = fmriprep_confounds[['a_comp_cor_00', 'a_comp_cor_01', 'a_comp_cor_02', 'a_comp_cor_03', 'a_comp_cor_04']]
-    aCompCor_confounds_trimmed = aCompCor_selection
-    output_aCompCor_file = BASEPATH + '{0}/confounds/aCompCor_all_{0}_ses-0{1}_run-02.txt'.format(sub_id, str(ses_nr))
-    aCompCor_confounds_trimmed.to_csv(output_aCompCor_file, sep = ' ', header = None, index = False)
 
-    # Addition of non-aggressive AROMA signal components to nuisance file for in-model non-aggressive AROMA
-    aroma_noise_ics = glob.glob(FMRIPREP_PATH + '{0}/ses-mri0{1}/func/*RS*run-2*AROMAnoiseICs*'.format(sub_id, str(ses_nr)))[0]
-    aroma_noise_ics_text = np.loadtxt(aroma_noise_ics, delimiter = ',', dtype = int).tolist()
-    aroma_noise_ics_text = [ic - 1 for ic in aroma_noise_ics_text]
-    mixing = pd.read_csv(glob.glob(FMRIPREP_PATH + '{0}/ses-mri0{1}/func/*RS_*run-2*MELODIC_mixing.tsv'.format(sub_id, str(ses_nr)))[0], sep = '\t', header = None)
-    mixing = mixing.drop(aroma_noise_ics_text, 1)
+    # SELECTION OF AROMA COMPONENTS
+    mixing = sub.get_aroma_confounds(session=ses_nr, run=2, task='RS')
     mixing_output_file = BASEPATH + \
-                         '{0}/confounds/Resting_State_confounds_all_{0}_ses-0{1}_run-02_nonaggrAROMA.txt'.format(sub_id, str(ses_nr))
-    mixing.to_csv(mixing_output_file, sep = ' ', header = None, index = False)
-    
-    combination_confounds_mixing = pd.concat([fmriprep_confounds_trimmed,mixing], axis = 1)
-    output_confound_file = BASEPATH + \
-                           '{0}/confounds/Resting_State_confounds_all_{0}_ses-0{1}_run-02_combined+nonaggrAROMA.txt'.format(sub_id, str(ses_nr))
-    combination_confounds_mixing.to_csv(output_confound_file, sep = ' ', header = None, index = False)
-    
-    # Addition of aCompCor
-    combination_confounds_aCompCor = pd.concat([fmriprep_confounds_trimmed, aCompCor_confounds_trimmed], axis = 1)
-    output_aCompCor_file_2 = BASEPATH + \
-       '{0}/confounds/Resting_State_confounds_all_{0}_ses-0{1}_run-02_combined+aCompCor.txt'.format(sub_id, str(ses_nr))
-    combination_confounds_aCompCor.to_csv(output_aCompCor_file_2, sep = ' ', header = None, index = False)
-    
-    # Retroicor Part
-    sub_physio = sub.get_physio(session = ses_nr-1, run = 2, task = 'RS')
-    sub_physio_np = np.array(sub_physio)
-    
-    sub_physio_columns = sub_physio.columns.tolist()
-    for regressor in sub_physio_columns:
-        single_reg = sub_physio[regressor]
-        np.savetxt(BASEPATH + '{0}/confounds/{1}'.format(sub_id, regressor), single_reg)
+                         '{0}/confounds/rs_confounds_{0}_ses-0{1}_run-02_aroma.csv'.format(sub_id, str(ses_nr))
+    mixing.to_csv(mixing_output_file,
+                  sep=' ',
+                  header=None,
+                  index=False)
 
-    # Multiplication Term
-    multi_list = []
-    multi_01 = sub_physio_np[:,0] * sub_physio_np[:,10]
-    multi_list.append(multi_01)
-    multi_02 = sub_physio_np[:,0] * sub_physio_np[:,11]
-    multi_list.append(multi_02)
-    multi_03 = sub_physio_np[:,1] * sub_physio_np[:,10]
-    multi_list.append(multi_03)
-    multi_04 = sub_physio_np[:,1] * sub_physio_np[:,11]
-    multi_list.append(multi_04)
+    for aroma_confound in mixing:
+        mixing[aroma_confound].to_csv(BASEPATH + '{0}/confounds/{1}'.format(sub_id , aroma_confound),
+                               sep=' ',
+                               header=None,
+                               index=False)
+
+    # COMBINATION OF ACOMPCOR AND AROMA
+    combination_confounds_mixing = pd.concat([fmriprep_confound_selection, mixing], axis=1)
+    output_confound_file = BASEPATH +\
+                           '{0}/confounds/rs_confounds_{0}_ses-0{1}_run-02_cosines_aroma.csv'.format(sub_id, str(ses_nr))
+
+    combination_confounds_mixing.to_csv(output_confound_file,
+                                        sep=' ',
+                                        header=None,
+                                        index=False)
+    # COMBINATION OF COSINES AND ACOMPCOR
+    combination_confounds_aCompCor = pd.concat([fmriprep_confound_selection, acompcor_confounds_trimmed], axis=1)
+    output_aCompCor_file_2 = BASEPATH + \
+       '{0}/confounds/rs_confounds_{0}_ses-0{1}_run-02_cosines_aCompCor.csv'.format(sub_id, str(ses_nr))
+
+    combination_confounds_aCompCor.to_csv(output_aCompCor_file_2,
+                                          sep=' ',
+                                          header=None,
+                                          index=False)
     
-    for mult_counter, multiplications in enumerate(multi_list):
-        np.savetxt(BASEPATH + '{0}/confounds/multiplication_term_CPRP_0{1}.txt'.format(sub_id, mult_counter + 1), multiplications)
-    
-    # Aroma Part
-    sub_confounds = sub.get_confounds(session = ses_nr - 1, run = 2, task = 'RS')
-    confounds_column_index = sub_confounds.columns.tolist()
-    aroma_sum = sum((itm.count("aroma_motion") for itm in confounds_column_index))
-    aroma_variables = confounds_column_index[-aroma_sum:]
-    for aroma_confounds in aroma_variables:
-        confound_selection = sub_confounds[aroma_confounds]
-        np.savetxt(BASEPATH + '{0}/confounds/{1}'.format(sub_id , aroma_confounds), confound_selection)
-        
-    #aCompCor Part
-    for aCompCor_confounds in aCompCor_confounds_trimmed:
-        aCompCor_c = aCompCor_confounds_trimmed[aCompCor_confounds]
-        np.savetxt(BASEPATH + '{0}/confounds/{1}'.format(sub_id , aCompCor_confounds), aCompCor_c)
+    # RETROICOR SELECTION
+    sub_physio = sub.get_physio(session=ses_nr,
+                                run=2,
+                                task='RS')
+
+    for regressor in sub_physio:
+        sub_physio[regressor].to_csv(BASEPATH + f'{sub_id}/confounds/{regressor}.csv')
+
+    sub_mult = sub.get_retroicor_confounds(session=ses_nr,
+                                           run=2,
+                                           task='RS')
+    for multiplication in sub_mult[sub_mult.columns[-4:]]:
+        sub_mult[sub_mult.columns[-4:]][multiplication].to_csv(BASEPATH + f'{sub_id}/confounds/{multiplication}.csv',
+                                        sep=' ',
+                                        header=None,
+                                        index=False)
