Updated approach to align with fix to RETORICOR computation

GLM_approach/GLM_02_run.py
@@ -19,19 +19,16 @@ print(sys.argv[1])
 sub_id = sys.argv[1]
 
 BASEPATH = '/project/3013068.03/physio_revision/GLM_approach/'
-"""part_list = glob.glob(BASEPATH + 'sub-*')
-part_list.sort() 
-part_list = [part_list[0]]
-"""
+
 # Indicating subject having the 'stress' condition during their FIRsT functional session
 stress_list = ['sub-002', 'sub-003', 'sub-004', 'sub-007', 'sub-009', 'sub-013', 'sub-015',
                'sub-017', 'sub-021', 'sub-023', 'sub-025', 'sub-027', 'sub-029']
 cor_ls = ['fdr', 'bonferroni']
 
 # Invoke subject_Class to allow access to all necessary data
-glm_path = '/project/3013068.03/physio_revision/GLM_approach/{0}/glm_output/'.format(sub_id)
+glm_path = '/project/3013068.03/physio_revision_fixed/GLM_approach/{0}/glm_output/'.format(sub_id)
 sub = Subject(sub_id)
-
+print(sub.retroicor_regressors)
 # Account for balancing in stress/control session order
 ses_nr = 2 if sub_id in stress_list else 1
 
@@ -48,7 +45,8 @@ mni_mask = sub.get_brainmask(MNI=True, session=ses_nr, run=2)
 melodic_GLM = glm.first_level.FirstLevelModel(t_r=2.02,
                                               slice_time_ref=None,
                                               smoothing_fwhm=6,
-                                              drift_model=None,
+                                              drift_model='cosine',
+                                              high_pass=0.008,
                                               hrf_model=None,
                                               mask_img=mni_mask,
                                               verbose=1)
@@ -144,10 +142,10 @@ glm3_report = glm_output.generate_report(contrasts=[F_contrast_acompcor],
 glm3_report.save_as_html(f'{glm_path}/{sub_id}_report_glm3.html')
 
 for output_type in F_contrast_acompcor_output.keys():
-    F_contrast_aroma_output[output_type].to_filename(glm_path + f'glm3_acompcor/acompcor_{output_type}.nii.gz')
+    F_contrast_acompcor_output[output_type].to_filename(glm_path + f'glm3_acompcor/acompcor_{output_type}.nii.gz')
 
 for correction in cor_ls:
-    thresholded_contrast = threshold_stats_img(F_contrast_aroma_output['z_score'],
+    thresholded_contrast = threshold_stats_img(F_contrast_acompcor_output['z_score'],
                                                alpha=.05,
                                                height_control=correction)
     nib.save(thresholded_contrast[0], glm_path + f'glm3_acompcor/acompcor_z_score_{correction}_corrected.nii.gz')
@@ -336,3 +334,55 @@ for contrast in glm7_contrast_dic.keys():
         nib.save(thresholded_contrast[0],
                  glm_path + f'glm7_retro_addition_aroma_acompcor/{contrast}_z_score_{correction}_corrected.nii.gz')
 
+
+# GLM HR
+
+design_glm8 = full_physio[full_physio.columns[-5:-2]]
+hr_contrast_length = np.shape(full_physio[full_physio.columns[-5:-3]])[1]
+design_glm8['constant'] = constant
+design_glm8.index = frame_times
+glm_output = melodic_GLM.fit(func_data, design_matrices=design_glm8)
+contrast_matrix = np.eye(design_glm8.shape[1])
+F_contrast_hr = contrast_matrix[:hr_contrast_length]
+F_contrast_hr_output = glm_output.compute_contrast([F_contrast_hr],
+                                                         stat_type='F',
+                                                         output_type='all')
+glm8_report = glm_output.generate_report(contrasts=[F_contrast_hr],
+                                         title=f'{sub_id} GLM8',
+                                         plot_type='glass')
+glm8_report.save_as_html(f'{glm_path}/{sub_id}_report_glm8.html')
+
+for output_type in F_contrast_hr_output.keys():
+    F_contrast_hr_output[output_type].to_filename(glm_path + f'glm8_hr/hr_{output_type}.nii.gz')
+
+for correction in cor_ls:
+    thresholded_contrast = threshold_stats_img(F_contrast_hr_output['z_score'],
+                                               alpha=.05,
+                                               height_control=correction)
+    nib.save(thresholded_contrast[0], glm_path + f'glm8_hr/hr_z_score_{correction}_corrected.nii.gz')
+
+# GLM RVT
+
+design_glm9 = full_physio[full_physio.columns[-2:]]
+rvt_contrast_length = np.shape(full_physio[full_physio.columns[-2:]])[1]
+design_glm9['constant'] = constant
+design_glm9.index = frame_times
+glm_output = melodic_GLM.fit(func_data, design_matrices=design_glm9)
+contrast_matrix = np.eye(design_glm9.shape[1])
+F_contrast_rvt = contrast_matrix[:rvt_contrast_length]
+F_contrast_rvt_output = glm_output.compute_contrast([F_contrast_rvt],
+                                                   stat_type='F',
+                                                   output_type='all')
+glm9_report = glm_output.generate_report(contrasts=[F_contrast_rvt],
+                                         title=f'{sub_id} GLM8',
+                                         plot_type='glass')
+glm9_report.save_as_html(f'{glm_path}/{sub_id}_report_glm9.html')
+
+for output_type in F_contrast_rvt_output.keys():
+    F_contrast_rvt_output[output_type].to_filename(glm_path + f'glm9_rvt/rvt_{output_type}.nii.gz')
+
+for correction in cor_ls:
+    thresholded_contrast = threshold_stats_img(F_contrast_rvt_output['z_score'],
+                                               alpha=.05,
+                                               height_control=correction)
+    nib.save(thresholded_contrast[0], glm_path + f'glm9_rvt/rvt_z_score_{correction}_corrected.nii.gz')

GLM_approach/GLM_05_aroma_misclassification.py
@@ -16,7 +16,7 @@ from nilearn.glm import threshold_stats_img
 import matplotlib.pyplot as plt
 import sys
 
-BASEPATH = '/project/3013068.03/physio_revision/GLM_approach/'
+BASEPATH = '/project/3013068.03/physio_revision_fixed/GLM_approach/'
 
 # Pools all available summary files
 
@@ -67,13 +67,13 @@ frame_times = np.arange(n_scans) * t_r
 mni_mask = sub.get_brainmask(MNI=True, session=ses_nr, run=2)
 
 # GLM settings
-melodic_GLM = glm.first_level.FirstLevelModel(t_r = 2.02,
-                                              slice_time_ref = 0,
-                                              smoothing_fwhm = 6,
-                                              drift_model = None,
-                                              hrf_model = None,
-                                              mask_img = mni_mask,
-                                              verbose = 1)
+melodic_GLM = glm.first_level.FirstLevelModel(t_r=2.02,
+                                              slice_time_ref=None,
+                                              smoothing_fwhm=6,
+                                              drift_model=None,
+                                              hrf_model=None,
+                                              mask_img=mni_mask,
+                                              verbose=1)
 
 
 # Loading respective functional run as NII-img-like nibabel object
@@ -102,6 +102,7 @@ columns_AROMA = ['AR_' + x[-2:] for x in aroma_noise]
 # Column name constant
 column_constant = ['Constant']
 
+"""
 # Account for subject without potential misclassification
 if potential_misclass_matrix:
     for component_id, component in enumerate(potential_misclass_matrix):
@@ -145,12 +146,12 @@ if potential_misclass_matrix:
              'melodic_misclassifications/{1}_fwe_corrected.nii.gz'.format(sub_id,
                                                                                                        'AddComp_' + str(potential_misclass_id[component_id]+1)))
         # Thresholded maps FDR
-        thresholded_RETRO_FWE, threshold_RETRO_FWE = threshold_stats_img(F_RETRO_unique_added_comp_output,
+        thresholded_RETRO_FDR, threshold_RETRO_FDR = threshold_stats_img(F_RETRO_unique_added_comp_output,
                                                                          alpha=.05,
                                                                          height_control='fdr')
 
         # Save resulting z-maps FDR-thresholded 0.05
-        nib.save(thresholded_RETRO_FWE, BASEPATH + '{0}/'\
+        nib.save(thresholded_RETRO_FDR, BASEPATH + '{0}/'\
              'melodic_misclassifications/{1}_fdr_corrected.nii.gz'.format(sub_id,
                                                                                                        'AddComp_' + str(potential_misclass_id[component_id]+1)))
 
@@ -160,7 +161,34 @@ if potential_misclass_matrix:
                                   threshold=None,
                                   title=sub_id + 'AddComp: ' + str(potential_misclass_id[component_id] + 1),
                                   output_file=BASEPATH + '{0}/'\
-                                  'melodic_misclassifications//{1}_fwe_corrected.png'\
+                                  'melodic_misclassifications/{1}_fwe_corrected.png'\
                                   .format(sub_id, 'AddComp_' + str(potential_misclass_id[component_id] + 1)),
                                   plot_abs=False)
         plt.close()
+"""
+if potential_misclass_matrix:
+    # Combine all components into single matrix
+    all_components = pd.concat(potential_misclass_matrix, axis=1)
+    component_names = [f'AddComp_{id + 1}' for id in potential_misclass_id]
+
+    # Create unified design matrix
+    design = pd.concat([all_components, retro_noise, aroma_noise], axis=1)
+    design['Constant'] = constant
+    design.columns = component_names + columns_RETRO + columns_AROMA + column_constant
+
+    # Create GLM with full design matrix
+    design.index = frame_times
+    glm_output = melodic_GLM.fit(func_data, design_matrices=design)
+
+    # Create contrast matrix for RETROICOR (accounts for all added components)
+    n_added = len(component_names)
+    contrast_matrix = np.eye(design.shape[1])
+    F_RETRO_contrast = contrast_matrix[n_added:n_added + len(columns_RETRO)]
+
+    # Compute F-test for RETROICOR variance
+    F_test = glm_output.compute_contrast(F_RETRO_contrast, stat_type='F')
+
+    # Save and threshold results (existing code adapted)
+    nib.save(F_test, f'{BASEPATH}{sub_id}/full_model_RETROICOR_F.nii.gz')
+    thresholded_FWE, _ = threshold_stats_img(F_test, alpha=.05, height_control='bonferroni')
+    nib.save(thresholded_FWE, f'{BASEPATH}{sub_id}/full_model_RETROICOR_FWE.nii.gz')

GLM_approach/GLM_07_zchange_calculation.py
@@ -10,9 +10,9 @@ import nibabel as nib
 import pandas as pd
 from nilearn import plotting
 import matplotlib.pyplot as plt
-
+'''
 # Path to data
-BASEPATH = '/project/3013068.03/physio_revision/GLM_approach/'
+BASEPATH = '/project/3013068.03/physio_revision_fixed/GLM_approach/'
 
 #Path to FDR corrected model of unique RETROICOR variance
 participant_list = glob.glob(BASEPATH + 'sub-*/glm_output/glm5_retro_aroma/'
@@ -41,7 +41,7 @@ for subject in participant_list:
     # Add z-maps for uncorrected addition of potential misclassifications
     zmaps_melodic_added = glob.glob(BASEPATH + f'{sub_id}/melodic_misclassifications/AddComp*uncorrected.nii.gz')
     zmaps_melodic_added.sort()
-
+    zmap_voxel_number = np.count_nonzero(zmap_masked.data)
     # Check whether there has been possible misclassifications detected previously for this subject
     if zmaps_melodic_added != []:
 
@@ -55,17 +55,20 @@ for subject in participant_list:
 
             melodic_added_masked = np.ma.array(np.nan_to_num(melodic_added_data, neginf=0, posinf=0),
                                                mask=zmap_mat)
-            masked_difference = zmap_masked - melodic_added_masked
+            masked_difference = melodic_added_masked - zmap_masked
             masked_difference_mean = masked_difference.mean()
 
             # Create a binarised mask nii-img
-            component_number = [int(s) for s in zmaps_melodic_added[zmap_counter][zmaps_melodic_added[zmap_counter].find('AddComp'):].split('_') if s.isdigit()]
+            component_number = \
+                [int(s) for s in zmaps_melodic_added[zmap_counter][zmaps_melodic_added[zmap_counter].
+                                                                   find('AddComp'):].split('_') if s.isdigit()]
 
             # Calculate z_change index
             z_change_list[zmap_counter] = masked_difference_mean
             component_list[zmap_counter] = component_number[0]
 
-            z_change_frame = pd.DataFrame({'Z Change': z_change_list, 'Melodic Component': component_list, 'Subject': [sub_id]*len(z_change_list)})
+            z_change_frame = pd.DataFrame({'Z Change': z_change_list, 'Melodic Component': component_list,
+                                           'Subject': [sub_id]*len(z_change_list), 'Voxel Number': zmap_voxel_number})
 
         #Create overall index
         index_list.append(z_change_frame)
@@ -74,7 +77,7 @@ for subject in participant_list:
 overall_z_change = pd.concat(index_list)
 overall_z_change.sort_values('Z Change',
                              inplace=True,
-                             ascending=False)
+                             ascending=True)
 overall_z_change.to_csv(BASEPATH + 'misclassification_zchange_overview.txt',
                         index=False)
 
@@ -88,11 +91,11 @@ for index, lines in overall_z_change.iterrows():
 
 
 # Create plot for all misclassifications sorted by z_change
-fig, axes = plt.subplots(nrows=14,
+fig, axes = plt.subplots(nrows=5,
                          ncols=2,
                          figsize=[20, 40])
 
-for component_counter, component in enumerate(component_list[:28]):
+for component_counter, component in enumerate(component_list[:10]):
     plotting.plot_glass_brain(component,
                               colorbar=True,
                               threshold=None,
@@ -100,7 +103,67 @@ for component_counter, component in enumerate(component_list[:28]):
                               axes=axes[int(component_counter / 2), int(component_counter % 2)],
                               annotate=False,
                               plot_abs=False)
-    print('{}% Done!'.format(component_counter / len(component_list) * 100))
+    print('{}%'.format(int(component_counter + 1 / 10) * 100))
 
-plt.savefig('/project/3013068.03/physio_revision/GLM_approach/overall_z_change_misclassifications.png')
+plt.savefig('/project/3013068.03/physio_revision_fixed/GLM_approach/overall_z_change_misclassifications.png')
 
+'''
+
+BASEPATH = '/project/3013068.03/physio_revision_fixed/GLM_approach/'
+
+# Load unified model results
+participant_list = glob.glob(BASEPATH + 'sub-*/full_model_RETROICOR_FWE.nii.gz')
+participant_list.sort()
+
+# Initialize data storage
+results = []
+
+for subject_path in participant_list:
+    sub_id = subject_path.split('/')[-2]
+
+    # Load unified model result
+    unified_zmap = nib.load(subject_path).get_fdata()
+
+    # Load original RETROICOR map (from previous processing)
+    original_path = f"{BASEPATH}{sub_id}/glm_output/glm5_retro_aroma/unique_retro_z_score_bonferroni_corrected.nii.gz"
+    original_zmap = nib.load(original_path).get_fdata()
+
+    # Calculate difference (unified model vs original)
+    z_diff = unified_zmap - original_zmap
+
+    # Create mask of significant voxels in original model
+    original_mask = original_zmap > 0
+
+    # Calculate metrics
+    voxel_count = np.count_nonzero(original_mask)
+    mean_z_change = z_diff[original_mask].mean() if voxel_count > 0 else 0
+
+    # Store results
+    results.append({
+        'Subject': sub_id,
+        'Voxel Number': voxel_count,
+        'Z Change': mean_z_change,
+        'Z-Map Path': subject_path
+    })
+
+# Create DataFrame and save
+overall_z_change = pd.DataFrame(results)
+overall_z_change.sort_values('Z Change', ascending=True, inplace=True)
+overall_z_change.to_csv(BASEPATH + 'unified_model_zchange_overview.txt', index=False)
+
+# Visualization
+fig, axes = plt.subplots(nrows=5, ncols=2, figsize=(20, 40))
+
+for idx, (_, row) in enumerate(overall_z_change[:10].iterrows()):
+    z_map = nib.load(row['Z-Map Path'])
+    title = f"{row['Subject']} | ZΔ: {row['Z Change']:.2f} | Voxels: {row['Voxel Number']}"
+
+    plotting.plot_glass_brain(z_map,
+                              colorbar=True,
+                              threshold=None,
+                              title=title,
+                              axes=axes[idx // 2, idx % 2],
+                              plot_abs=False)
+
+plt.savefig(f'{BASEPATH}unified_model_zchange_comparison.png')
+plt.close()